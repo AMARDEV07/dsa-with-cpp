96f69cefeb4f1d8a612a937fb6b8cb5aa52b3ccd
Amar dev panwar <127836087+AMARDEV07@users.noreply.github.com>
2024-05-09 23:36:46 +0530

    Add files via upload

---
+++ DAY2/2DATATYPE.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a=5;
+char grade='a';
+bool isAman=true;
+float prize=1.4;
+double no=6;
+
+cout<< "a is ="<<a <<endl << "size is="<<sizeof(int)<<endl;
+cout<< "grade is="<<grade<<endl<< "size is ="<<sizeof(char)<<endl;
+
+cout<< "is aman is="<<isAman<<endl<< "size is= "<<sizeof(bool)<<endl;
+cout<< "grade is="<<prize  <<endl<< "size is= "<<sizeof(float)<<endl;
+cout<< "grade is="<<no <<endl<< "size is ="<<sizeof(double)<<endl;
+    
+    }  
+    
---
+++ DAY3/operators.cpp
@@ -0,0 +1,46 @@
+//arthematic
+//+,-,/,++,--
+
+//assigne =
+//comparision <,><=
+//logical &&
+
+
+
+#include<iostream>
+using namespace std;
+ int main()
+{
+
+    int a=4;
+     int b=3;
+
+     if((a<b) && (a==b)){
+        cout<<"ho gya";
+     }
+     else{
+        cout<<"nahi huwa";
+     }
+
+    // cout<<a+b;
+    // cout<<a-b;
+    // cout<<a/b;
+    // cout<<a*b;
+
+
+//     int a=4;
+//    int  b=a++;
+//    int c=7;
+//    int d=--c;
+
+//    cout<<a;
+//    cout<<b<<endl;
+
+//    cout<<c;
+//    cout<<d;
+    
+
+
+
+return 0;
+}
